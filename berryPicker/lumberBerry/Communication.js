0e67ee9267401fab6eaefe5a946677df6a1683a8
emce.dev <emce.dev@gmail.com>
2024-01-24 22:00:37 +0100

    fixes in communciaotn

--- berryPicker/lumberBerry/Communication.js
+++ berryPicker/lumberBerry/Communication.js
@@ -15,6 +15,7 @@ const { MuschroomFarmer } = require("../muschroomFarmer");
 const { MuschroomFarmerMax } = require("../mushroomFarmerMax");
 const { grinderAfk } = require("../attack");
 const { farmer } = require("../farmer");
+const { honeyMan } = require("../honeyMan");
 
 // const intents = new Intents
 let channel = undefined;
@@ -77,13 +78,16 @@ const initializeCommunication = () => {
         LumberJackMin(bot, dcSend);
       }
       if (content.startsWith("Honey")) {
-        LumberJackMin(bot, dcSend);
         const bot = await startBot(dcSend);
         await wait(4000);
-        bot.chat("/login minecraft123");
+        bot.chat(`/login ${pass}`);
         await wait(4000);
-        bot.chat("/is home bee");
-        await wait(4000);
+        for (let i = 0; i < 100000; i++) {
+          bot.chat("/is home bee");
+          await wait(4000);
+          await honeyMan(bot, dcSend);
+          await wait(240000);
+        }
       }
       if (content.startsWith("Guard")) {
         const bot = await startBot(dcSend);
