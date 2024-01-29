9500a9eac20c04b5cf990b999579b1dae3f4c21d
emce.dev <emce.dev@gmail.com>
2024-01-29 14:21:20 +0100

    communication changes

--- berryPicker/lumberBerry/Communication.js
+++ berryPicker/lumberBerry/Communication.js
@@ -117,10 +117,10 @@ const initializeCommunication = () => {
 
         await grinderAfk(bot, dcSend);
       }
-      if (content.startsWith("find")) {
+      if (content.startsWith("Find")) {
         const bot = await startBot(dcSend);
         await wait(4000);
-        bot.chat("/login minecraft123");
+        bot.chat(`/login ${pass}`);
         await wait(4000);
         bot.chat("/zmienserwer skyblock");
         await wait(4000);
