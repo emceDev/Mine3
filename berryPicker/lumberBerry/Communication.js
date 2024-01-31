1c6c7084db6444c47885308875617a187137c83a
emce.dev <emce.dev@gmail.com>
2024-01-31 17:25:34 +0100

    fixed algorithm sending

--- berryPicker/lumberBerry/Communication.js
+++ berryPicker/lumberBerry/Communication.js
@@ -147,7 +147,7 @@ const initializeCommunication = () => {
           profit += await MuschroomFarmer(bot, dcSend).catch((err) =>
             dcSend("sth fcked up")
           );
-          if (i / 10) {
+          if (i % 10) {
             dcSend(profit);
           }
           console.log("profit", profit);
