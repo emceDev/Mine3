b61af6d581a3966b3e704c0bfbf316cf2aa43b11
emce.dev <emce.dev@gmail.com>
2024-02-03 20:38:15 +0100

    eneabled alerta

--- berryPicker/Utils/config.js
+++ berryPicker/Utils/config.js
@@ -7,8 +7,8 @@ const pass = "jestGrane1";
 // const username = "Berserker321";
 // const pass = "minecraft123";
 
-const host = "127.0.0.1";
-// const host = "mcosada.pl";
+// const host = "127.0.0.1";
+const host = "mcosada.pl";
 // const host = "iskyblock.pl";
 const version = "1.20.1";
 
@@ -17,7 +17,7 @@ const channelId = "833669360069509160";
 const safeMovements = {
   allowSprinting: false,
   allowParkour: false,
-  canDig: false,
+  canDig: true,
   allow1by1towers: false,
   maxDropDown: 2,
   blocksCantBreak: new Set([9, 8, 4, 46]),
