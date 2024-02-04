013a36f961eeb8690fffda0f11a4c644170e6972
emce.dev <emce.dev@gmail.com>
2024-02-04 16:13:22 +0100

    pathfinder tewakrs

--- berryPicker/farmer/index.js
+++ berryPicker/farmer/index.js
@@ -135,12 +135,11 @@ const farmer = async (bot, dcSend) => {
           const ifGo =
             plantArea[i - 1] && checkX(plantArea[i - 1], plantArea[i], toggle);
           if (ifGo === true) {
-            console.log("GOING true");
+            console.log("GOING true", i);
             await go(bot, block, 1, safeMovements);
           }
           if (plant.name === "air") {
             notPlanted++;
-
             await sowPlant("potato", block);
             plantedNow++;
           } else {
